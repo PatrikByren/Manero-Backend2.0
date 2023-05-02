257e85f652c2e1adb3e130f2300f85bddc8ab8a3
carllohr <calle.lohr@gmail.com>
2023-05-02 19:08:38 +0200

    orderstatus models, shippingorderentity

--- Manero/Models/BaseModels/Order.cs
+++ Manero/Models/BaseModels/Order.cs
@@ -1,12 +1,16 @@
 ﻿using Manero.Interfaces;
+using Manero.Models.Entities;
 
 namespace Manero.Models.BaseModels
 {
-    public class Order : IOrder
+    public abstract class Order : IOrder
     {
+        public int Id { get; set; }
         public DateTime OrderDate { get; set; }
         public int ShippingAddressId { get; set; }
         public string UserId { get; set; } = null!;
         public decimal OrderTotal { get; set; }
+        public int OrderStatusId { get; set; }
+        public OrderStatusEntity OrderStatus { get; set; } = null!;
     }
 }
---
+++ Manero/Models/Entities/ShippingMethodEntity.cs
@@ -0,0 +1,6 @@
+﻿namespace Manero.Models.Entities
+{
+    public class ShippingMethodEntity
+    {
+    }
+}
