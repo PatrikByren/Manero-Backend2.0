3277360e8244bd07b36d4b0e738618c780c1ab94
carllohr <calle.lohr@gmail.com>
2023-05-02 11:52:47 +0200

    real ordercontroller

--- Manero/Controllers/OrderController.cs
+++ Manero/Controllers/OrderController.cs
@@ -1,12 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Manero.Controllers
 {
-    public class OrderController : Controller
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
     }
 }
